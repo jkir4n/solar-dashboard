a9e5b5d7e348e139b48d7b1712b54b933e301c17
Kiran J <jkir4n@gmail.com>
2026-05-19 22:51:41 +0530

    chore: weather-fx.js

--- src/weather-fx.js
+++ src/weather-fx.js
@@ -370,7 +370,7 @@ export class WeatherFX {
     this._ensureFogParticles();
   }
 
-  start(weatherCondition, isNight, theme = 'dark', windSpeed = 0, moonBrightness = 0, moonElevation = -90, moonAzimuth = 180, sunElevation = -90, sunAzimuth = 180, cloudCoverage = null, windBearing = 180, visibility = null, precipIntensity = null, thunderstormProb = null, heatIndex = null, windChill = null) {
+  start(weatherCondition, isNight, theme = 'dark', windSpeed = 0, moonBrightness = 0, moonElevation = -90, moonAzimuth = 180, sunElevation = -90, sunAzimuth = 180, cloudCoverage = null, windBearing = 180, visibility = null, precipIntensity = null, thunderstormProb = null, heatIndex = null, windChill = null, uvIndex = null, humidity = null, temperature = null) {
     this._theme = theme;
     this._isNight = isNight;
     this._windSpeed = windSpeed;
